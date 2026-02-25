b160e99704ce2b8393d20439f5202bc4b7601f93
hax1zi <davidfontes303@gmail.com>
2026-02-25 16:07:35 -0300

    feat: criando user entity e seu arquivo de teste

---
+++ src/users/domain/entities/__tests__/unit/user.entity.spec.ts
@@ -0,0 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
+import { faker } from '@faker-js/faker';
+import { UserEntity, UserProps } from '../../user.entity';
+
+describe('UserEntity unit tests', () => {
+  let props: UserProps;
+  let sut: UserEntity;
+
+  beforeEach(() => {
+    props = {
+      name: faker.name.fullName(),
+      email: faker.internet.email(),
+      password: faker.internet.password(),
+    };
+
+    sut = new UserEntity(props);
+  });
+
+  it('Constructor method', () => {
+    expect(sut.props.name).toEqual(props.name);
+    expect(sut.props.email).toEqual(props.email);
+    expect(sut.props.password).toEqual(props.password);
+    expect(sut.props.createdAt).toBeInstanceOf(Date);
+  });
+});
